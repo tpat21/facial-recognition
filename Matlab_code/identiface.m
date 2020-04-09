c1eaeb8a2527283d142ad12f499c12146d6beaf4
tpat21 <62774678+tpat21@users.noreply.github.com>
2020-04-09 18:51:58 -0400

    Delete identiface.m

--- Matlab_code/identiface.m
+++
@@ -1,9 +0,0 @@
-function [s,conf] = identiface(Q,R,subj,z)
-% Q and R are the reduced QR factors of A
-%The other input is another image z in vector form
-
-
-%The first output s is the subject number (not column number) that is 
-    %judged to be the most likely match for z,
-%The second output conf is a number between 0 (lowest) and 1 (highest) that 
-    %indicates the level of confidence in the identification.
